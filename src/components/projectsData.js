277fabf05c794e2c95f974abe44f09d3f8f34718
Om <omspatil980@gmail.com>
2025-01-05 01:52:53 +0530

    rearrange project

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -3,15 +3,6 @@
 const projectsData = [
   {
     id: 1,
-    name: "Weather App",
-    description:
-      "A React-based weather app that fetches live weather data using an API.",
-    technologies: ["React", "CSS", "REST API"],
-    githubLink: "https://github.com/om151/WeatherApp",
-    liveLink: "https://weatherapp-om151s-projects.vercel.app/",
-  },
-  {
-    id: 2,
     name: "Hotel Listing Platform",
     description:
       "A web application where users can list hotels and leave reviews.",
@@ -20,13 +11,21 @@ const projectsData = [
     liveLink: "https://vistastay-6yv0.onrender.com/listings",
   },
   {
-    id: 3,
-    name: "Simon Game",
+    id: 2,
+    name: "Weather App",
     description:
-      "A memory skill game where players repeat increasingly long sequences of lights and sounds.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    githubLink: "https://github.com/om151/Simon_game",
-    liveLink: "https://om151.github.io/Simon_game",
+      "A React-based weather app that fetches live weather data using an API.",
+    technologies: ["React", "CSS", "REST API"],
+    githubLink: "https://github.com/om151/WeatherApp",
+    liveLink: "https://weatherapp-om151s-projects.vercel.app/",
+  },
+  {
+    id:3,
+    name: "Personal portfolio website",
+    description: "My personal portfolio website to showcase my skills, projects, and achievements.",
+    technologies: ["HTML", "CSS", "React.js", "JavaScript"],
+    githubLink: "https://www.ompatil.dev/",
+    liveLink: "https://www.ompatil.dev",
   },
   {
     id: 4,
@@ -37,14 +36,17 @@ const projectsData = [
     githubLink: "https://github.com/om151/CricketScoreboard",
     liveLink: "https://om151.github.io/CricketScoreboard/",
   },
-  {
-    id:5,
-    name: "Personal portfolio website",
-    description: "My personal portfolio website to showcase my skills, projects, and achievements.",
-    technologies: ["HTML", "CSS", "React.js", "JavaScript"],
-    githubLink: "https://www.ompatil.dev/",
-    liveLink: "https://www.ompatil.dev",
-  }
+  // {
+  //   id: 5,
+  //   name: "Simon Game",
+  //   description:
+  //     "A memory skill game where players repeat increasingly long sequences of lights and sounds.",
+  //   technologies: ["HTML", "CSS", "JavaScript"],
+  //   githubLink: "https://github.com/om151/Simon_game",
+  //   liveLink: "https://om151.github.io/Simon_game",
+  // },
+  
+  
 ];
 
 export default projectsData;
